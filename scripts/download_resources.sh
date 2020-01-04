43c727a3d33b18f34e975e435e9d6bd1376cf773
Degang Wu <dwuab@alumni.ust.hk>
2020-01-04 21:37:27 +0800

    added script for downloading resource files

---
+++ scripts/download_resources.sh
@@ -0,0 +1,36 @@
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
+
+printf "downloading beagle genetic maps ...\n"
+
+if [ ! -d $DIR/resources/geneticMap_GRCh37 ]; then
+        mkdir $DIR/resources/geneticMap_GRCh37
+fi
+
+pushd $DIR/resources/geneticMap_GRCh37
+
+wget http://bochet.gcc.biostat.washington.edu/beagle/genetic_maps/plink.GRCh37.map.zip
+
+unzip plink.GRCh37.map.zip
+
+rm plink.GRCh37.map.zip
+
+popd
+
+if [ ! -d $DIR/pipelines/varCall/gotcloud.ref/ ]; then
+	mkdir $DIR/pipelines/varCall/gotcloud.ref/
+fi
+
+printf "downloading gotcloud bundles ...\n"
+
+pushd $DIR/pipelines/varCall/gotcloud.ref/
+
+wget ftp://anonymous@share.sph.umich.edu/gotcloud/ref/hs37d5-db142-v1.tgz
+
+printf "uncompressing hs37d5-db142-v1.tgz...\n"
+
+tar zxvf hs37d5-db142-v1.tgz
+
+rm hs37d5-db142-v1.tgz
+
